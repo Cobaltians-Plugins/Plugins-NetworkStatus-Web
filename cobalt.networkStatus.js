70d5e2b63ed3074c006078637f3734e36064fe92
= <tristan.daniel.kristal@gmail.com>
2016-04-14 16:13:22 +0200

    Merge 'state' and 'type' as 'status', add status enum

--- cobalt.networkStatus.js
+++ cobalt.networkStatus.js
@@ -3,18 +3,28 @@
         name: 'networkStatus',
 
         defaultHandlers: {
-            onStatusChanged: function (data) {
-                cobalt.log('Network status updated: ' + data);
+            onStatusChanged: function (status) {
+                cobalt.log('Network status updated: ' + status);
             }
         },
 
+        status: {
+            WIFI: 'wifi',
+            MOBILE: 'mobile',
+            ETHERNET: 'ethernet',
+            VPN: 'vpn',
+            BLUETOOTH: 'bluetooth',
+            UNKNOWN: 'unknown',
+            NONE: 'none'
+        },
+
         init: function (options) {
-            cobalt.network = {
-                getState: this.getState.bind(this),
-                getType: this.getType.bind(this),
+            cobalt.networkStatus = {
+                getStatus: this.getStatus.bind(this),
                 startStatusMonitoring: this.startStatusMonitoring.bind(this),
                 stopStatusMonitoring: this.stopStatusMonitoring.bind(this),
-                onStatusChanged: this.defaultHandlers.onStatusChanged
+                onStatusChanged: this.defaultHandlers.onStatusChanged,
+                status: this.status
             };
 
             this.defineCallbacks(options);
@@ -22,7 +32,7 @@
 
         defineCallbacks: function (options) {
             if (options && typeof options.onStatusChanged == 'function')
-                cobalt.network.onStatusChanged = options.onStatusChanged;
+                cobalt.networkStatus.onStatusChanged = options.onStatusChanged;
         },
 
         startStatusMonitoring: function (options) {
@@ -33,29 +43,20 @@
             this.send('stopStatusMonitoring');
         },
 
-        getState: function (callback) {
-            this.send('getState', {}, function (data) {
+        getStatus: function (callback) {
+            this.send('getStatus', {}, function (data) {
                 if (typeof callback == 'function')
-                    callback(data.state);
+                    callback(data.status);
                 else
                     cobalt.log('Received network status: ', data);
-            });
-        },
-
-        getType: function (callback) {
-            this.send('getType', {}, function (data) {
-                if (typeof callback == 'function')
-                    callback(data.type);
-                else
-                    cobalt.log('Received network type: ', data);
             });
         },
 
         handleEvent: function (json) {
             switch (json && json.action) {
                 case 'onStatusChanged':
-                    if (typeof cobalt.network.onStatusChanged == 'function')
-                        cobalt.network.onStatusChanged(json.data);
+                    if (typeof cobalt.networkStatus.onStatusChanged == 'function')
+                        cobalt.networkStatus.onStatusChanged(json.data.status);
                     break;
 
                 default:
