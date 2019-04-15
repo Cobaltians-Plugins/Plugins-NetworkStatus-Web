f48e68300ebe4ebda509c85de4651ba4650e67c8
Guillaume Gendre <dzb.rtz@gmail.com>
2019-04-15 10:17:51 +0200

    ready to handle event

--- cobalt.networkStatus.js
+++ cobalt.networkStatus.js
@@ -1,64 +1,53 @@
-(function (cobalt) {
-    var plugin = {
-        classes: {
-        	ios:'CobaltNetworkStatusPlugin',
-			android: 'io.kristal.networkstatusplugin.NetworkStatusPlugin'
-        },
-        defaultHandlers: {
-            onStatusChanged: function (status) {
-                cobalt.log('Network status updated: ' + status);
-            }
-        },
+(function(cobalt) {
+  var plugin = {
+    classes: {
+      ios: 'CobaltNetworkStatusPlugin',
+      android: 'io.kristal.networkstatusplugin.NetworkStatusPlugin'
+    },
+    defaultHandlers: {
+      onStatusChanged: function(status) {
+        cobalt.log('Network status updated: ' + status);
+      }
+    },
 
-        status: {
-            WIFI: 'wifi',
-            MOBILE: 'mobile',
-            UNKNOWN: 'unknown',
-            NONE: 'none'
-        },
+    status: {
+      WIFI: 'wifi',
+      MOBILE: 'mobile',
+      UNKNOWN: 'unknown',
+      NONE: 'none'
+    },
 
-        init: function () {
-            cobalt.networkStatus = {
-                getStatus: this.getStatus.bind(this),
-                startMonitoring: this.startMonitoring.bind(this),
-                stopMonitoring: this.stopMonitoring.bind(this),
-                onStatusChanged: this.defaultHandlers.onStatusChanged,
-                status: this.status
-            };
-        },
-        startMonitoring: function (callback) {
-            if (typeof callback == 'function')
-                cobalt.networkStatus.onStatusChanged = callback;
+    init: function() {
+      cobalt.networkStatus = {
+        getStatus: this.getStatus.bind(this),
+        startMonitoring: this.startMonitoring.bind(this),
+        stopMonitoring: this.stopMonitoring.bind(this),
+        onStatusChanged: this.defaultHandlers.onStatusChanged,
+        status: this.status
+      };
+    },
+    startMonitoring: function(callback) {
+      cobalt.networkStatus.onStatusChanged = callback;
+      cobalt.plugins.send(this, 'startStatusMonitoring');
+    },
 
-            cobalt.plugins.send(this, 'startStatusMonitoring');
-        },
+    stopMonitoring: function() {
+      cobalt.plugins.send(this, 'stopStatusMonitoring');
+    },
 
-        stopMonitoring: function () {
-            cobalt.plugins.send(this, 'stopStatusMonitoring');
-        },
+    getStatus: function(callback) {
+      cobalt.networkStatus.onStatusChanged = callback;
+      cobalt.plugins.send(this, 'getStatus', {});
+    },
 
-        getStatus: function (callback) {
-            cobalt.plugins.send(this, 'getStatus', {}, function (data) {
-                if (typeof callback == 'function')
-                    callback(data.status);
-                else
-                    cobalt.log('Received network status: ', data);
-            });
-        },
-
-        handleEvent: function (json) {
-            switch (json && json.action) {
-                case 'onStatusChanged':
-                    if (typeof cobalt.networkStatus.onStatusChanged == 'function')
-                        cobalt.networkStatus.onStatusChanged(json.data.status);
-                    break;
-
-                default:
-                    cobalt.log(this.name, ': unknown action ', json.action);
-                    break;
-            }
-        }
-    };
-
-    cobalt.plugins.register(plugin);
+    handleEvent: function(json) {
+      switch (json && json.action) {
+        case 'onStatusChanged':
+          if (typeof cobalt.networkStatus.onStatusChanged === 'function')
+            cobalt.networkStatus.onStatusChanged(json.data.status);
+          break;
+      }
+    }
+  };
+  cobalt.plugins.register(plugin);
 })(cobalt || {});
